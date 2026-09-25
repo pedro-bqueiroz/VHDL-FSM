24de1ee12c87093d00de6e4593cebe2cbdd863e5
Pedro Queiroz <pedrobirroqz@gmail.com>
2026-09-25 14:18:53 -0300

    Update fsm.vhd

--- fsm.vhd
+++ fsm.vhd
@@ -1,40 +1,3 @@
--- Divisor de clock com entrada de 50MHz e saída de 1Hz
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity DivisorClock is
-port ( clk50MHz : in std_logic;
-       reset : in std_logic;
-       clk0d2Hz : out std_logic
-     );
-end DivisorClock;
-
-architecture Behavioral of DivisorClock is
-
---signal count : integer := 0;
-signal b : std_logic := '0';
-begin
-
--- Geração do Clock. Para um clock de 50MHz esse process gera um sinal de clock de 0.2Hz.
-process(clk50MHz, b)
-	variable cnt : integer range 0 to 2**27-1;
-	begin
-		if(rising_edge(clk50MHz)) then
-		   if(reset = '1') then
-			   cnt := 0;
-			else
-			   cnt := cnt + 1;
-         end if;
-			if(cnt = 124999999) then
-				b <= not b;
-				cnt := 0;
-			end if;
-		end if;
-		clk0d2Hz <= b;
-	end process;
-end;
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -86,13 +49,6 @@ architecture fsm of my_fsm is
   signal x : STD_LOGIC;
   
 begin
-
-  DIV : entity work.DivisorClock
-    port map (
-      clk50MHz => Clk,
-      reset => Reset,
-      clk0d2Hz => x 
-  );
 
   sync_proc : process (CLK) -- note: no NS here!
   begin -- state reset and transition
